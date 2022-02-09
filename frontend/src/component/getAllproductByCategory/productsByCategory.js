0c766e716aa749e3d9d10aee22caa58cfe26d8ec
NasserQasim <nasseralotaiby1@gmail.com>
2022-02-09 13:37:17 +0200

    prepare the starter code

---
+++ frontend/src/component/getAllproductByCategory/productsByCategory.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  setproducts,
+  addproduct,
+  updateproduct,
+  deleteproduct,
+  getproductsByState,
+} from "../reducer/products/index";
+import { BiShowAlt } from "react-icons/bi";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
